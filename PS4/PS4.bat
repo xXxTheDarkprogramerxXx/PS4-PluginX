ea4d892b0a0b9541c97eef0ba90e982110f1cdc5
xXxTheDarkprogramerxXx <eonvwyk@gmail.com>
2021-12-17 09:10:32 +0200

    Delete PS4 directory

--- PS4/PS4.bat
+++
@@ -1,4 +0,0 @@
-pushd "%~dp0"
-orbis-ctrl pkill eboot.bin
-orbis-run /fsroot . /console:process /log:"eboot.log" /elf "eboot.bin"
-popd
--- PS4/PS4_NEO.bat
+++
@@ -1,7 +0,0 @@
-pushd "%~dp0"
-orbis-ctrl pkill eboot.bin
-echo ^<settings version="1" target="orbis"^> ^<setting key="0x7802B600"  value="0"  /^> ^</settings^> > %tmp%\ps4_settings_allowNEOmode.xml
-orbis-ctrl settings-import %tmp%\ps4_settings_allowNEOmode.xml
-del %tmp%\ps4_settings_allowNEOmode.xml
-orbis-run /fsroot . /console:process /log:"eboot.log" /elf "eboot.bin"
-popd
